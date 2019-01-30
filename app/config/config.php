c922e8fd87c6d861e1190c7af02b2b5f4dd66c46
Fresp <root@fresp.tech>
2019-01-30 22:54:14 +0000

    Prepared Project

---
+++ app/config/config.php
@@ -0,0 +1,40 @@
+<?php 
+require_once APPPATH.'/config/common.config.php'; // Common configuration
+require_once APPPATH.'/config/db.config.php'; // Database configuration
+require_once APPPATH.'/config/i18n.config.php'; // i18n configuration
+
+// ASCII Secure random crypto key
+define("CRYPTO_KEY", "def000006fa2218022416186fc66ac5aaae794e0a3a4a92cb50b3107df1da34c138706c2e686acf5dde9fa52e2565928f76adc5ec634e92257fd4fad2c260c63a0b0430c");
+
+// General purpose salt
+define("NP_SALT", "KduV3EEltAbLQLeX");
+
+
+// Path to instagram sessions directory
+define("SESSIONS_PATH", APPPATH . "/sessions");
+// Path to temporary files directory
+define("TEMP_PATH", ROOTPATH . "/assets/uploads/temp");
+
+
+// Path to themes directory
+define("THEMES_PATH", ROOTPATH . "/inc/themes");
+// URI of themes directory
+define("THEMES_URL", APPURL . "/inc/themes");
+
+
+// Path to plugins directory
+define("PLUGINS_PATH", ROOTPATH . "/inc/plugins");
+// URI of plugins directory
+define("PLUGINS_URL", APPURL . "/inc/plugins");
+
+// Path to ffmpeg binary executable
+// NULL means it's been installed on global path
+// If you set the value other than null, then it will only be 
+// validated during posting the videos
+define("FFMPEGBIN", NULL);
+
+// Path to ffprobe binary executable
+// NULL means it's been installed on global path
+// If you set the value other than null, then it will only be 
+// validated during posting the videos
+define("FFPROBEBIN", NULL);
